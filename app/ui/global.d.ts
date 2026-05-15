b1fdaeb67d52b358739f5ba2798ed0eb458a00bd
MeleshkoYuliya <Yuliya.Meleshko@ventionteams.com>
2026-05-15 13:11:16 +0300

    Added project

---
+++ app/ui/global.d.ts
@@ -0,0 +1,11 @@
+declare module "*.css";
+declare module "*.woff";
+declare module "*.woff2";
+declare module "*.ttf";
+declare module "*.eot";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
